03b822aff3920395d2cb44499e685abd6f1ae8e1
profe <profe@HX99G>
2024-11-21 17:13:28 -0800

    Add project files.

---
+++ Components/Models/Product.cs
@@ -0,0 +1,11 @@
+﻿namespace LearnBlazor.Components.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public required string Name { get; set; }
+
+        public decimal Price { get; set; }
+        public bool IsActive { get; set; }
+    }
+}
