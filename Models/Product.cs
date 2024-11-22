dedfb62df5706b2753b679658ee52fad0ad8fe81
Oladele Femi <profemzy@gmail.com>
2024-11-21 22:57:13 -0800

    Refactor Classes and update menu

--- Models/Product.cs
+++ Models/Product.cs
@@ -4,7 +4,6 @@
     {
         public int Id { get; set; }
         public required string Name { get; set; }
-
         public decimal Price { get; set; }
         public bool IsActive { get; set; }
         public required IEnumerable<ProductProp> ProductProperties { get; set; } = new List<ProductProp>();
